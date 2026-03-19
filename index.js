70b57385a35fa424a0c4e024a9c4784b4a3ce4ef
naodesign2025 <crosstalk9011@yahoo.co.jp>
2026-03-19 18:00:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.post("/register",async (req, res)=> {
             console.error(err);
             res.status(500).send(`登録に失敗しました<br><a href="/">戻る</a>`);
         }
-
+});
 
 
 app.listen(PORT,()=> {
